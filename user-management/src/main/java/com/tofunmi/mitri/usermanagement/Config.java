4215f45e294b9fbc52279e0e9f593e8c848311b4
tofunmi <tofunmiseun@gmail.com>
2023-01-31 19:50:26 +0000

    google ID token verifier is now able to verify multiple clients

--- user-management/src/main/java/com/tofunmi/mitri/usermanagement/Config.java
+++ user-management/src/main/java/com/tofunmi/mitri/usermanagement/Config.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Collections;
+import java.util.List;
 
 /**
  * Created By tofunmi on 12/07/2022
@@ -17,9 +17,12 @@ import java.util.Collections;
 public class Config {
 
     @Bean
-    public GoogleIdTokenVerifier googleIdTokenVerifier(@Value("${google-client-id}") String googleClientId) {
+    public GoogleIdTokenVerifier googleIdTokenVerifier(
+            @Value("${google-web-client-id}") String googleWebClientId,
+            @Value("${google-ios-client-id}") String googleIosClientId
+    ) {
         return new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
-                .setAudience(Collections.singletonList(googleClientId))
+                .setAudience(List.of(googleWebClientId, googleIosClientId))
                 .build();
     }
 }
