7af768aa08f429c5e3c0a8a73f3a3434efef2229
tofunmi <tofunmiseun@gmail.com>
2023-04-23 00:08:26 +0100

    updates to a chat is now published to online users

--- web-service/src/main/java/com/tofunmi/mitri/webservice/chat/OnlineChatProfilesHandler.java
+++ web-service/src/main/java/com/tofunmi/mitri/webservice/chat/OnlineChatProfilesHandler.java
@@ -1,7 +1,6 @@
-package com.tofunmi.mitri.webservice;
+package com.tofunmi.mitri.webservice.chat;
 
 import lombok.NonNull;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -9,35 +8,35 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 @Component
-public class ChatSocketHandler extends TextWebSocketHandler {
-    Map<String, WebSocketSession> map = Collections.synchronizedMap(new HashMap<>());
+public class OnlineChatProfilesHandler extends TextWebSocketHandler {
+    Map<String, WebSocketSession> subscribedProfiles = Collections.synchronizedMap(new HashMap<>());
 
     @Override
     public void afterConnectionEstablished(@NonNull WebSocketSession session) throws Exception {
         String profileId = getProfileId(session);
-        map.put(profileId, session);
+        subscribedProfiles.put(profileId, session);
     }
 
     @Override
     public void afterConnectionClosed(@NonNull WebSocketSession session, @NonNull CloseStatus status) throws Exception {
-        map.remove(getProfileId(session));
+        subscribedProfiles.remove(getProfileId(session));
     }
 
     private String getProfileId(WebSocketSession session) {
         return Objects.requireNonNull(session.getUri()).getQuery().replace("profileId=", "");
     }
 
-    @Scheduled(fixedRate = 6_000)
-    public void doSomethingToConnectedSessions() throws IOException {
-        for (var session : map.values()) {
-            if (session.isOpen()) {
-                session.sendMessage(new TextMessage("Hi, how may we help you?"));
+    public void notifyProfiles(Collection<String> profileIds) {
+        for (String profileId : profileIds) {
+            if (subscribedProfiles.containsKey(profileId)) {
+                WebSocketSession session = subscribedProfiles.get(profileId);
+                try {
+                    session.sendMessage(new TextMessage("reload"));
+                } catch (IOException ignored) {
+                }
             }
         }
     }
