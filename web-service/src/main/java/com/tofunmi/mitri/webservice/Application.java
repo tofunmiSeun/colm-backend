e7906d580233e5415737c2ab9e4837b00a3e85c7
tofunmi <tofunmiseun@gmail.com>
2023-04-22 23:04:21 +0100

    web sockets scaffolding is now more tangible

--- web-service/src/main/java/com/tofunmi/mitri/webservice/Application.java
+++ web-service/src/main/java/com/tofunmi/mitri/webservice/Application.java
@@ -3,11 +3,13 @@ package com.tofunmi.mitri.webservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 @SpringBootApplication(scanBasePackages = {"com.tofunmi.mitri"})
 @EnableMongoRepositories(basePackages = {"com.tofunmi.mitri"})
 @EnableWebSocket
+@EnableScheduling
 public class Application {
 
 	public static void main(String[] args) {
--- web-service/src/main/java/com/tofunmi/mitri/webservice/ChatSocketHandler.java
+++ web-service/src/main/java/com/tofunmi/mitri/webservice/ChatSocketHandler.java
@@ -1,29 +1,44 @@
 package com.tofunmi.mitri.webservice;
 
+import lombok.NonNull;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 @Component
 public class ChatSocketHandler extends TextWebSocketHandler {
-    Map<String, WebSocketSession> map = new HashMap<>();
+    Map<String, WebSocketSession> map = Collections.synchronizedMap(new HashMap<>());
 
     @Override
-    public void handleTextMessage(WebSocketSession session, TextMessage message) {
-        String x = new String(message.asBytes());
-        map.put(x, session);
+    public void afterConnectionEstablished(@NonNull WebSocketSession session) throws Exception {
+        String profileId = getProfileId(session);
+        map.put(profileId, session);
+    }
+
+    @Override
+    public void afterConnectionClosed(@NonNull WebSocketSession session, @NonNull CloseStatus status) throws Exception {
+        map.remove(getProfileId(session));
+    }
+
+    private String getProfileId(WebSocketSession session) {
+        return Objects.requireNonNull(session.getUri()).getQuery().replace("profileId=", "");
     }
 
     @Scheduled(fixedRate = 6_000)
-    public void runnAh() throws IOException {
-        for (var x : map.keySet()) {
-            map.get(x).sendMessage(new TextMessage("Hi, how may we help you?"));
+    public void doSomethingToConnectedSessions() throws IOException {
+        for (var session : map.values()) {
+            if (session.isOpen()) {
+                session.sendMessage(new TextMessage("Hi, how may we help you?"));
+            }
         }
     }
 }
